7f009198a8bd3228428b3f761baa5ddef49434ae
ChadlinMaalgraaff <53153872+ChadlinMaalgraaff@users.noreply.github.com>
2020-05-14 16:59:35 +0200

    Component boilerplate code added

---
+++ src/Components/ArtistProfile/ArtistProfile.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class ArtistProfile extends Component {
+
+    render() {
+        return (
+            <h1>ArtistProfile</h1>
+        );
+    }
+}
+
+export default ArtistProfile;
---
+++ src/Components/ArtpieceProfile/ArtProfile.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class ArtProfile extends Component {
+
+    render() {
+        return (
+            <h1>ArtProfile</h1>
+        );
+    }
+}
+
+export default ArtProfile;
